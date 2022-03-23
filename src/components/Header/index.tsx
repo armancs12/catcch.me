6c2db680976da4853453bc3945d92976e439011b
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-24 00:10:31 +0300

    Install react-bootstrap and Create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,53 @@
+import styles from "./Header.module.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Logo from "../Logo";
+import cx from "classnames";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const HeaderLink = (props: { href: string; text: string }) => {
+  const { pathname } = useRouter();
+  const isActive = pathname === props.href;
+
+  return (
+    <Link href={props.href} passHref>
+      <Nav.Link active={isActive}>
+        {props.text}
+      </Nav.Link>
+    </Link>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <Navbar
+      className={cx(styles.Header)}
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+    >
+      <Container className={styles.HeaderContainer}>
+        <Link href="/" passHref>
+          <Navbar.Brand>
+            <Logo size="small" />
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className={styles.ResponsiveNavbar}
+        >
+          <Nav className="me-auto">
+            <HeaderLink href="/about" text="About" />
+          </Nav>
+          <Nav>
+            <HeaderLink href="/login" text="Login" />
+            <HeaderLink href="/register" text="Register" />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Header from "../components/Header";
+import { Container, SSRProvider } from "react-bootstrap";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SSRProvider>
       <Head>
         <title>catcch.me - Dead Simple Error Tracking</title>
         <meta
@@ -13,8 +17,11 @@ function App({ Component, pageProps }: AppProps) {
         />
         <link rel="icon" href="/icons/octopus.svg" />
       </Head>
-      <Component {...pageProps} />
-    </>
+      <Header />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </SSRProvider>
   );
 }
 
