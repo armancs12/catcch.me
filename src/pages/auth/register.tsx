74efb60fccc2c0800af58a6ab4f594d7fbc14d65
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-24 12:04:01 +0300

    Create register page

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import AuthForm from "../../components/AuthForm";
+import styles from "../../styles/RegisterPage.module.css"
+
+const RegisterPage: NextPage = () => {
+  return (
+    <main>
+      <AuthForm
+        className={styles.Form}
+        title="Register"
+        description={
+          <>
+            Register to use the app for free without credit card. You can update
+            if you want to use it in more than 3 projects. (
+            <Link href={"/about"}>Learn more</Link>)
+          </>
+        }
+        emailButtonText="Register with Email"
+        googleButtonText="Register with Google"
+      />
+      <p className={styles.FormFooter}>Already have an account? <Link href="/auth/login">Login</Link></p>
+    </main>
+  );
+};
+
+export default RegisterPage;
