989c4efa855fc84fe61974f718959a5c27227157
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-27 15:18:37 +0300

    Update Header to work with next-auth

--- src/client/components/Header/index.tsx
+++ src/client/components/Header/index.tsx
@@ -4,6 +4,7 @@ import Logo from "../Logo";
 import cx from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 const HeaderLink = (props: { href: string; text: string }) => {
   const { pathname } = useRouter();
@@ -11,14 +12,16 @@ const HeaderLink = (props: { href: string; text: string }) => {
 
   return (
     <Link href={props.href} passHref>
-      <Nav.Link active={isActive}>
-        {props.text}
-      </Nav.Link>
+      <Nav.Link active={isActive}>{props.text}</Nav.Link>
     </Link>
   );
 };
 
 const Header: React.FC = () => {
+  const { data, status } = useSession();
+  const isLoading = status === "loading";
+  const isAuthenticated = status === "authenticated" && data != null;
+
   return (
     <Navbar
       className={cx(styles.Header)}
@@ -40,10 +43,21 @@ const Header: React.FC = () => {
           <Nav className="me-auto">
             <HeaderLink href="/about" text="About" />
           </Nav>
-          <Nav>
-            <HeaderLink href="/auth/login" text="Login" />
-            <HeaderLink href="/auth/register" text="Register" />
-          </Nav>
+          {!isLoading &&
+            (isAuthenticated ? (
+              <Nav>
+                <HeaderLink
+                  href="/auth/profile"
+                  text={data.user?.name ?? data.user?.email ?? "Profile"}
+                />
+                <HeaderLink href="/auth/logout" text="Logout" />
+              </Nav>
+            ) : (
+              <Nav>
+                <HeaderLink href="/auth/login" text="Login" />
+                <HeaderLink href="/auth/register" text="Register" />
+              </Nav>
+            ))}
         </Navbar.Collapse>
       </Container>
     </Navbar>
