8cc9c08b4518bacaac28620cb3a3f9b20bee5752
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-30 16:15:46 +0300

    Fix too many connection error

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -1,9 +1,21 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 let database: PrismaClient | undefined;
 
-if (!database) {
-    database = new PrismaClient();
+declare global {
+  var database: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === "production") {
+  database = new PrismaClient({ log: ["info", "query", "error", "warn"] });
+} else {
+  if (!global.database) {
+    global.database = new PrismaClient({
+      log: ["info", "query", "error", "warn"],
+    });
+  }
+
+  database = global.database;
 }
 
 export default database as PrismaClient;
