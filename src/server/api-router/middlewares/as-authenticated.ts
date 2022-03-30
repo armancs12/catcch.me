543e9ba130f23c9708117189340ed41d044cbd58
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-30 16:16:55 +0300

    Await promise

--- src/server/api-router/middlewares/as-authenticated.ts
+++ src/server/api-router/middlewares/as-authenticated.ts
@@ -2,13 +2,13 @@ import createHttpError from "http-errors";
 import { getSession } from "next-auth/react";
 import { APIMiddleware } from "../@types";
 
-const asAuthenticated: APIMiddleware = (req, res, next) => {
-    const session = getSession({ req });
-    if (session != null) {
-        return next();
-    }
+const asAuthenticated: APIMiddleware = async (req, res, next) => {
+  const session = await getSession({ req });
+  if (session != null) {
+    return next();
+  }
 
-    throw createHttpError(401, "Access denied! Please login to continue.");
+  throw createHttpError(401, "Access denied! Please login to continue.");
 };
 
 export default asAuthenticated;
