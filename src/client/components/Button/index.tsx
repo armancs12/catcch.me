da410aece088b593f545190b312d81ca40853729
Serhat S <47033919+serhatsdev@users.noreply.github.com>
2022-03-31 18:30:42 +0300

    Update Button component

--- src/client/components/Button/index.tsx
+++ src/client/components/Button/index.tsx
@@ -7,13 +7,20 @@ type Props = {
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   type?: "reset" | "submit" | "button";
+  wide?: boolean;
 };
 
-const Button: React.FC<Props> = ({ children, className, onClick, type }) => {
+const Button: React.FC<Props> = ({
+  children,
+  className,
+  onClick,
+  type,
+  wide = false,
+}) => {
   return (
     <BootstrapButton
       variant="dark"
-      className={cx(styles.Button, className)}
+      className={cx(styles.Button, className, wide && styles.ButtonWide)}
       onClick={onClick}
       type={type}
     >
--- src/pages/auth/logout.tsx
+++ src/pages/auth/logout.tsx
@@ -1,14 +1,16 @@
 import { asAuthenticated } from "@client/auth";
 import Box from "@client/components/Box";
 import Button from "@client/components/Button";
-import { signOut } from "next-auth/react"
+import { signOut } from "next-auth/react";
 
 const LogoutPage = () => {
-    return (
-        <Box title="Logout" description="Are you sure you want to logout?">
-            <Button onClick={() => signOut()}>Logout</Button>
-        </Box>
-    )
-}
+  return (
+    <Box title="Logout" description="Are you sure you want to logout?">
+      <Button onClick={() => signOut()} wide={true}>
+        Logout
+      </Button>
+    </Box>
+  );
+};
 
 export default asAuthenticated(LogoutPage);
